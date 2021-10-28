f95170eeb4b381665af2705abcd2f1b5089cb92b
anastasia <anastasia.aizman@gmail.com>
2021-10-28 16:13:26 -0400

    add - saving city selection in parent component

--- frontend/components/CitiesDropdown.js
+++ frontend/components/CitiesDropdown.js
@@ -1,16 +1,27 @@
 import axios from "axios";
 import React from "react";
 import Select from "react-select";
+import PropTypes from 'prop-types';
 
 const baseURL = "/cities";
 
 function formatCities(city) {
     // react-select expects {value: something, label: something}
-    return {value: city, label: city.toUpperCase()};
+    return {value: city, label: city.split("-").join(" ").toUpperCase()};
 }
+
 export default class CitiesDropdown extends React.Component {
+    static propTypes = {
+        selectedCity: PropTypes.string,
+        onChange: PropTypes.func
+    };
     state = {
         cities: []
+    }
+    changeHandler = (e) => {
+        if (typeof this.props.onChange === "function") {
+            this.props.onChange(e);
+        }
     }
 
     componentDidMount() {
@@ -23,6 +34,6 @@ export default class CitiesDropdown extends React.Component {
     }
 
     render() {
-        return <Select options={this.state.cities}/>;
+        return <Select onChange={this.changeHandler} options={this.state.cities}/>;
     }
 }
--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -2,26 +2,37 @@ import React from "react";
 import Plot from "react-plotly.js";
 import CitiesDropdown from "./CitiesDropdown.js";
 
-const Home = () => {
 
-    return (
-        <>
-            <CitiesDropdown/>
+export default class Home extends React.Component {
+    state = {
+        city: ""
+    }
+
+    changeHandler = (value) => {
+        this.setState({
+            city: value
+        });
+    }
+
+    render() {
+        return <>
+            <CitiesDropdown
+                onChange={this.changeHandler}
+            />
             <Plot
                 data={[
                     {
                         x: [1, 2, 3],
                         y: [2, 6, 3],
-                        type: 'scatter',
-                        mode: 'lines+markers',
-                        marker: {color: 'red'},
+                        type: "scatter",
+                        mode: "lines+markers",
+                        marker: {color: "red"}
                     },
-                    {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+                    {type: "bar", x: [1, 2, 3], y: [2, 5, 3]}
                 ]}
-                layout={{width: 600, height: 400, title: 'A Fancy Plot'}}
+                layout={{width: 600, height: 400, title: "A Fancy Plot"}}
             />
-        </>
-    );
-};
+        </>;
+    }
+}
 
-export default Home;
